24bdc35a003a58e61c085bd30869a2708dd23c30
Harald Schwab <hs@rigbit.at>
2022-06-29 22:10:47 +0200

    refactored dragover handling
    does not work when stopping dragover handler
    this version has no problems

--- src/app/drop-area/drop-area.component.ts
+++ src/app/drop-area/drop-area.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostListener, OnDestroy, OnInit, Renderer2 } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { preventStop } from '../../helper/event-handler/prevent-stop';
 import { UploadFile } from '../../helper/file/upload.file';
 import { DropService } from './drop.service';
@@ -8,15 +8,10 @@ import { DropService } from './drop.service';
   templateUrl: './drop-area.component.html',
   styleUrls: [ './drop-area.component.scss' ],
 })
-export class DropAreaComponent implements OnInit, OnDestroy {
-  private dragoverUnListener!: Function;
+export class DropAreaComponent implements OnInit {
   private isDragging: boolean = false;
 
-  constructor(
-    private renderer: Renderer2,
-    private ref: ElementRef,
-    private dropService: DropService,
-  ) { }
+  constructor(private dropService: DropService) { }
 
   public get background() {
     return this.isDragging ? '#bbb' : '#fff';
@@ -26,43 +21,29 @@ export class DropAreaComponent implements OnInit, OnDestroy {
     return this.isDragging ? 'drop' : 'drag';
   }
 
-  ngOnInit(): void {
-    this.dragoverListener();
-  }
+  ngOnInit(): void { }
 
-  ngOnDestroy(): void {
-    this.dragoverUnListener();
-  }
-
-  private dragoverListener() {
-    this.isDragging = false;
-    this.dragoverUnListener = this.renderer.listen(this.ref.nativeElement, 'dragover', (event: DragEvent) => {
-      preventStop(event);
-      this.isDragging = true;
-      this.dragoverUnListener();
-    });
+  @HostListener('dragover', [ '$event' ])
+  private dragover(event: DragEvent) {
+    preventStop(event);
+    this.isDragging = true;
   }
 
   @HostListener('dragleave', [ '$event' ])
   private dragleave(event: DragEvent) {
     preventStop(event);
-    this.dragoverListener();
+    this.isDragging = false;
   }
 
   @HostListener('drop', [ '$event' ])
   private drop(event: DragEvent) {
     preventStop(event);
-    console.log('drop', event.dataTransfer?.files);
     if ((event.dataTransfer?.files || []).length > 0) {
       const files = event.dataTransfer!.files;
       for (let i = 0; i < files.length; i++) {
-        const file = new UploadFile(files.item(i)!);
-        this.dropService.current = file;
-        file.loadContent()
-          .then(console.log)
-          .catch(console.error);
+        this.dropService.current = new UploadFile(files.item(i)!);
       }
     }
-    this.dragoverListener();
+    this.isDragging = false;
   }
 }
