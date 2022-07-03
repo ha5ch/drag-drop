541ad8c299d6438a1353514b5e8063fc651507bd
Harald Schwab <hs@rigbit.at>
2022-07-03 14:18:23 +0200

    add DirectoryComponent

--- src/app/directory-dragger/directory-dragger.component.ts
+++ src/app/directory-dragger/directory-dragger.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-directory',
+  selector: 'app-directory-dragger',
   templateUrl: './directory-dragger.component.html',
   styleUrls: ['./directory-dragger.component.scss']
 })
 export class DirectoryDraggerComponent implements OnInit {
+  public directories: string[] = [];
+
   constructor() { }
 
   ngOnInit(): void { }
+
+  add() {
+    const name = prompt("test");
+    if (name && name.trim().length > 0 && !this.directories.find(d => d === name)) {
+      this.directories.push(name);
+    }
+  }
 }
--- src/app/directory-dragger/directory-dragger.module.ts
+++ src/app/directory-dragger/directory-dragger.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DirectoryDraggerComponent } from './directory-dragger.component';
+import { DirectoryComponent } from './directory/directory.component';
 
 
 @NgModule({
   declarations: [
     DirectoryDraggerComponent,
+    DirectoryComponent,
   ],
   imports: [
     CommonModule,
