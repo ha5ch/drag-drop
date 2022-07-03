8c1f687a2020cffcd8a0efb1970c3804096fed8c
Harald Schwab <hs@rigbit.at>
2022-07-03 13:57:01 +0200

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { FileDraggerModule } from './file-dragger/file-dragger.module';
+import { FileDraggerComponent } from './file-dragger/file-dragger.component';
 
+const routes: Routes = [
+  { path: 'files', component: FileDraggerComponent },
+  { path: '', redirectTo: '/files', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
@@ -16,6 +22,7 @@ import { FileDraggerModule } from './file-dragger/file-dragger.module';
   imports: [
     BrowserModule,
     FileDraggerModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent],
